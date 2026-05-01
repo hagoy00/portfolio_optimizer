a5123d9c7ffe0116236e9fa1b2ede1fcf99234b2
hagoy00 <hagos_y@icloud.com>
2026-05-01 12:18:31 -0700

    Remove duplicate print statement in optimizer_core.py

--- utils/optimizer_core.py
+++ utils/optimizer_core.py
@@ -1,4 +1,5 @@
 print(">>> LOADED optimizer_core.py FROM:", __file__)
+print(">>> LOADED optimizer_core.py FROM:", __file__)
 def run_optimizer(prices):
     print(">>> RUN OPTIMIZER CALLED")   # Debug print
 

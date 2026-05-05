6b9eeaa24b4f0a3b3b8494ccb0ea5158a83ddff6
hagoy00 <hagos_y@icloud.com>
2026-05-05 13:51:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # ... all your other imports ...
+from datetime import date
 
 # ⬇️ PASTE THE STICKY HEADER CSS RIGHT HERE
 st.markdown("""

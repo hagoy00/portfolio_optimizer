80a4c8926b64eed605f22f9c02643d866d47e370
hagoy00 <hagos_y@icloud.com>
2026-05-08 20:11:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -260,7 +260,7 @@ tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9 = st.tabs([
     "AI Commentary",
     "Buy Analysis",
     "Optimizer"
-
+])
 # ---------------------------------------------------------
 # Overview
 # ---------------------------------------------------------

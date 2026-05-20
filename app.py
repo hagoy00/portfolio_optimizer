d3143eff10e63ff3fc6ee1d0621ba40598feb567
hagoy00 <hagos_y@icloud.com>
2026-05-20 15:02:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -317,21 +317,6 @@ else:
 valid_tickers = aligned_tickers
 weights = aligned_weights
 
-# ---------------------------------------------------------
-# REALIGN WEIGHTS TO MATCH RETURNS_DF COLUMNS
-# ---------------------------------------------------------
-aligned_tickers = [t for t in portfolio_tickers if t in returns_df.columns]
-
-aligned_weights = np.array([ticker_to_weight[t] for t in aligned_tickers], dtype=float)
-
-if aligned_weights.sum() > 0:
-    aligned_weights = aligned_weights / aligned_weights.sum()
-else:
-    aligned_weights = np.array([1 / len(aligned_tickers)] * len(aligned_tickers))
-
-valid_tickers = aligned_tickers
-weights = aligned_weights
-
 # DO NOT OVERWRITE valid_tickers
 # valid_tickers must remain portfolio_tickers
 
